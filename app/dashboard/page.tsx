72b89d2599bc12092a5acf26cdaccc7ba1f872a0
Jon Paino <jonpaino@g.ucla.edu>
2024-08-31 13:22:47 -0700

    Fixed import path for heatmap component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/authOptions";
+
+export default async function Page() {
+
+  const session = await getServerSession(authOptions);
+  console.log("Session : ", session);
+
     return <p>Dashboard Page</p>;
   }
