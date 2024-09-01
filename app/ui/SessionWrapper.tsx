abff19a395fe600d50a5a6649bf3067f6093e25f
Jon Paino <jonpaino@g.ucla.edu>
2024-09-01 09:02:17 -0700

    chore: Update SessionWrapper to use "/api/auth/signin" as the basePath

--- app/ui/SessionWrapper.tsx
+++ app/ui/SessionWrapper.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 
 const SessionWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-        <SessionProvider basePath="/api/auth">
+        <SessionProvider basePath="/api/auth/signin">
             {children}
         </SessionProvider>
     );
